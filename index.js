1a0ef4b64a11ed3296d7569f0e77e3bb9226f3b8
Harish Mallepalli <harishmallepalli1996@gmail.com>
2020-01-10 13:58:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // dct cors 
+const express = require("express")
+const app = express()
+const axios = require("axios")
+const cors = require("cors")
+
+app.use(cors())
+
+app.get("*",async (req,res)=>{
+    try{
+        const data = await axios.get(req.url.slice(1))
+        res.status(200).json(data.data)
+    }catch(err){
+        res.status(400).json(err.message)
+    }
+})
+
+
+
+app.listen(3000)
